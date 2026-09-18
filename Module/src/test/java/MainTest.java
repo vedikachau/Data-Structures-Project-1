143b5fbf28e376017342d92e1c32131a8ed28ac4
mteerElle <mteer@calpoly.edu>
2026-09-17 21:28:01 -0700

    Update MainTest.java

--- Module/src/test/java/MainTest.java
+++ Module/src/test/java/MainTest.java
@@ -9,4 +9,26 @@ class MainTest {
         assertEquals(45, Main.addSix(39));
     }
 
+    @Test
+    void addSession(){
+        MainGUI.Session test = new MainGUI.Session(1234,"whar","mr.dooks","2026-09-01","build 5",15,2);
+       // assertEquals();
+    }
+    @Test
+    void displaySession(){
+
+    }
+    @Test
+    void searchSession(){
+
+    }
+    @Test
+    void removeSession(){
+
+    }
+    @Test
+    void register(){
+
+    }
+
 }
